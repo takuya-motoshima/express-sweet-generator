6dd2e38cb03aa16d6a6426fc3e4cfc2d0ae881be
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2021-05-07 19:28:33 +0900

    Fix layout template.

--- templates/config/database.js
+++ templates/config/database.js
@@ -4,27 +4,36 @@
 module.exports = {
   development: {
     username: 'root',
+    password: null,
     database: 'example',
     host: 'localhost',
+    port: undefined,
     dialect: 'mariadb',
+    timezone: '+09:00',
     // timezone: 'Etc/GMT-9',
     logging: false,
     // dialectOptions: {useUTC: false, timezone: 'Etc/GMT-9'}
   },
   test: {
     username: 'root',
+    password: null,
     database: 'example',
     host: 'localhost',
+    port: undefined,
     dialect: 'mariadb',
+    timezone: '+09:00',
     // timezone: 'Etc/GMT-9',
     logging: false,
     // dialectOptions: {useUTC: false, timezone: 'Etc/GMT-9'}
   },
   production: {
     username: 'root',
+    password: null,
     database: 'example',
     host: 'localhost',
+    port: undefined,
     dialect: 'mariadb',
+    timezone: '+09:00',
     // timezone: 'Etc/GMT-9',
     logging: false,
     // dialectOptions: {useUTC: false, timezone: 'Etc/GMT-9'}
