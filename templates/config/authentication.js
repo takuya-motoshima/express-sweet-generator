b7df907739312d7f6861c4bdd964e10f70ef8837
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2021-05-31 13:45:30 +0900

    Fix basic configuration file.

--- templates/config/authentication.js
+++ templates/config/authentication.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/UserModel');
 
+
 /**
  * User authentication configuration interface.
  */
@@ -35,10 +36,37 @@ module.exports = {
   failure_redirect: '/login',
 
   /**
-   * Model class used for authentication, this is a required field.
-   * @type {typeof Model}
+   * This hook is called when authenticating a user.
+   * Please find the user information that owns the credentials based on the user name and password you received and return it.
+   * If the user who owns the credentials cannot be found, return null.
+   *
+   * Note that the user information must include an ID value that can identify the user.
+   * 
+   * @type {(username: string, password: string) => Promise<{[key: string]: any}|null>}
    */
-  model: UserModel,
+  authenticate_user: async (username, password) => {
+    return UserModel.findOne({
+      where: {
+        email: username,
+        password: password
+      },
+      raw: true
+    });
+  },
+
+  /**
+   * This hook is called when user authentication is successful.
+   * Please search and return the authenticated user information to be set in the session based on the user ID of the parameter.
+   * The returned data will be set in the req.user property and the view's session variable.
+   * 
+   * @type {(id: number|string) => Promise<{[key: string]: any}>}
+   */
+  subscribe_user: async (id) => {
+    return UserModel.findOne({
+      where: {id},
+      raw: true
+    });
+  },
 
   /**
    * URL without authentication. If the URL described in the access URL partially matches, authentication will not be performed, defaults to none.
--- templates/config/config.js
+++ templates/config/config.js
@@ -32,5 +32,47 @@ module.exports = {
    * The endpoint to run when the root URL is requested, defaults to none (undefined).
    * @type {string}
    */
-  default_router: '/home'
+  default_router: '/home',
+
+  /**
+   * This is a hook that rewrites the base URL.
+   * If you want to rewrite the app.locals.baseUrl property and the view's baseUrl variable, use this hook to return a new base URL.
+   * The default value is the referrer's origin (eg https://example.com).
+   *
+   * @example
+   * rewrite_base_url: baseUrl => {
+   *   return `${baseUrl}/admin`;
+   * }
+   * 
+   * @type {(baseUrl: string): string}
+   */
+  rewrite_base_url: baseUrl => {
+    return baseUrl;
+  },
+
+  /**
+   * This is a hook for error handling.
+   * For example, you can use it when you want to send an external notification of the error received by this hook.
+   * 
+   * @example
+   * error_handler: async err => {
+   *   // Notify system administrator of error.
+   *   return new Promise((resolve, reject) => {
+   *     const sendmail = require('sendmail')();
+   *     sendmail({
+   *       from: 'no-reply@example.com',
+   *       to: 'administrator@example.com',
+   *       subject: 'Error occurred',
+   *       text: err.message
+   *     }, err => {
+   *       err ? reject(err) : resolve();
+   *     });
+   *   });
+   * }
+   * 
+   * @type {(err: any): void|Promise<void>}
+   */
+  error_handler: err => {
+    console.error(`An error has occurred. Error message: ${err.message}`);
+  }
 }
